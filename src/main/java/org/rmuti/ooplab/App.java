f73ec8bd4a0641137e00c3622da9a6a7cfb4250d
chaluemwut <chaluemwut@hotmail.com>
2023-08-01 10:32:31 +0700

    first commit

---
+++ src/main/java/org/rmuti/ooplab/App.java
@@ -0,0 +1,13 @@
+package org.rmuti.ooplab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
